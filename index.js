ecd80b01eaf4200ce23cc77fa456ef2b5bbb23f3
slowwshy <arthurtoon13@gmail.com>
2026-03-09 00:13:59 -0300

    add prisma and mysql

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
 
 app.listen(3005);
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get('/', function(req, res){
    res.render('html');
+})
+
+app.post('/users', async () => {
+
+   const users = await prisma.user.findMany()
+
+   res.send(users)
 })
