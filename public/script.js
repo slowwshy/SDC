97bd3ec460d4f16650ee55a1a3d843acd072185e
slowwshy <arthurtoon13@gmail.com>
2026-03-10 23:04:54 -0300

    mecanica de jogo

--- public/script.js
+++ public/script.js
@@ -1,9 +1,48 @@
+
+
 function Start()
 {
-    var paper;
-    var rock;
-    var scissor;
+    
+    var PPT = ["pedra", "papel", "tesoura"]
     var endGame = false;
+    var score = 0
+    var text = document.getElementById("msg").value.toLowerCase().trim();
+    var res2 = document.getElementById("Emsg")
 
-    alert(endGame);
+    if(text === "pedra" || text ==="papel" || text === "tesoura"){
+       
+        var computador = PPT[Math.floor(Math.random() * PPT.length)];
+
+        if(text === computador)
+            {
+                res2.innerHTML = "Empate"
+            }
+        else if((text === "pedra" && computador === "tesoura") || (text === "tesoura" && computador === "papel") || (text === "papel" && computador === "pedra"))
+            {
+                res2.innerHTML = "vc ganhou"
+                score += 10
+                
+            }
+            else
+            {
+                endGame = true
+                res2.innerHTML = "vc perdeu acabou o jogo, seu score: " + score
+                
+                if(endGame = true)
+                {
+
+                    document.getElementById("button").disabled = true
+
+                }
+        
+            }
+            
+          
+    }
+    else
+    {
+        res2.innerHTML = "ERROR!!!"
+    }
+
+   
 }
