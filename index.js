e714d68c171662db1ad664885ac74c1adabf90a7
slowwshy <arthurtoon13@gmail.com>
2026-03-12 15:02:23 -0300

    page login + configs

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const {PrismaClient} = require("@prisma/client");
 const { where } = require('sequelize');
+const { error } = require('node:console');
 const prisma = new PrismaClient();
 
-app.listen(3005);
-
+app.listen(3005, ()=>{
+      if(app)
+      {
+       console.log("Server open")
+      }
+      else if(app = error)
+      {
+       console.log("ERROR!!!!!")
+      }
+      
+});
 app.set('view engine', 'ejs');
+
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
 app.get('/', async (req, res) => {
@@ -25,7 +37,22 @@ app.get('/', async (req, res) => {
  
 })
 
+app.get('/login', (req, res) =>{
+
+      res.render("login")
+})
+
 app.post('/users', async () => {
 
-  
+   const email = req.body.email
+   
+
+   await prisma.users.create({
+      data:{
+            name: email,
+            score: 0
+
+      }
+
+   })
 })
